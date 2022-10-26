9e185aba96703ddda057748c82dd6f3a5b2dcd21
Gael Touquet <gael.touquet@gmail.com>
2022-10-26 15:04:17 +0200

    forgot main

---
+++ Main.java
@@ -0,0 +1,11 @@
+package IsingModel;
+
+// spins that have their position in the table and in the space
+
+// table that has array of spins and handles finding neighbours
+
+// material has a table and can calculate all the properties of the material
+
+public class Main {
+    
+}
